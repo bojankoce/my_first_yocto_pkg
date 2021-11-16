059abf56caa02349f3373f50c48433bc80ed297e
Bojan Jovanovic <bojanjov@gmail.com>
2021-11-16 13:42:10 +0100

    Update publisher_member_function.cpp

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -38,7 +38,7 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, Swiftlane! " + std::to_string(count_++);
+    message.data = "Hello, Bojan! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
